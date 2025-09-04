af441bb7f187362069c84ddf5bfc68fd7021695d
shamshad chachu <chachushamshad0987@gmail.com>
2025-09-04 15:10:18 +0530

    changed des as desciption on mapping

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 
 const JobCard = ({job}) => {
   const [apply,setApply] =useState(false)
-    const shortDes = job.des
+    const shortDes = ((job.description).substring(0,100))
   return (
     <div className="p-6">
                   <h3 className="text-xl font-bold text-slate-800 mb-2">{job.title}</h3>
                   <p className="text-slate-600 text-sm mb-2">Experience: {job.experience}</p>
                   <p className="text-slate-600 text-sm mb-2">Location: {job.location}</p>
-                  <p className="text-slate-500 text-sm line-clamp-3">{shortDes.substring(0,100)}</p>
+                  <p className="text-slate-500 text-sm line-clamp-3">{shortDes}</p>
                   <div className="mt-4">
                     <a className={`w-full text-center inline-block ${apply?"bg-green-300":"bg-blue-600"} ${apply?"hover:bg-green-700":"hover:bg-blue-700"}
                      text-white font-bold py-2 px-4 rounded-lg transition duration-150 ease-in-out`} onClick={()=>{setApply(true)}} >
